55ea42a8a0841cb6309bb8fd8ab0f1f6bf874ee1
Diego-Lopes <diegolopes2009@hotmail.com>
2025-01-18 00:00:52 -0400

    🐛fix: removed component table customer

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,3 @@
-import CustomersTable from "@/app/ui/customers/table";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -6,5 +5,5 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return <CustomersTable customers={[]}/>
+  return <p>Customer Page</p>
 }
